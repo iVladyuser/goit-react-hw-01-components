1063617308c64ae998143d918d48323a56a84d67
iVladyuser <korol.v09@gmail.com>
2023-10-14 00:14:53 +0200

    transaction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,9 @@ import data from './Data/data.json';
 import FriendList from './FriendList/FriendList';
 import friends from './Data/friends.json';
 
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from './Data/transactions.json';
+
 export const App = () => {
   return (
     <>
@@ -19,7 +22,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
-    {/* <TransactionHistory items={transactions} /> */}
+    <TransactionHistory items={transactions} />
     </>
   );
 };
